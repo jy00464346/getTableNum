1beba069d15df2c05cd5286884744d1d5d9bd040
muzhongyuan <muzhonguan@wanda.cn>
2016-10-24 22:49:11 +0800

    first

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let puid = &args[1];
+        println!("puid={}", puid);
+        println!("tableNum={number:>0width$}", number=get_table_num(puid),width=3);
+    } else {
+        println!("{} , input puid args ", &args[0]);
+    }
+}
+
+fn get_table_num(puid: &str) -> i32 {
+    let mut hash = 0 as i32;
+    for c in puid.chars() {
+        print!("{}", c);
+        hash = (hash * 31) + c as i32;
+    }
+    ( hash % 256).abs()
+}
